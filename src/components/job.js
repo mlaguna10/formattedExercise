a1eb1a9bfefc2b25b63cf565c437e01a19404480
Mateo Laguna <m.laguna10@uniandes.edu.co>
2022-11-22 09:18:37 -0500

    first commit

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {FormattedDate} from 'react-intl';
+import {FormattedNumber} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
 
 const Job = (props) => {
   return (
@@ -6,9 +9,17 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+      {props.offer.salary} &nbsp;
+      <FormattedPlural value={props.offer.salary} one="millón" other="millones" />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+      <FormattedDate value={new Date(props.offer.date)} year='numeric' month='long' day='numeric' weekday='long'/>
+      </td>
+      <td>
+      <FormattedNumber value={props.offer.views}/>
+      </td>
     </tr>
   );
 };
